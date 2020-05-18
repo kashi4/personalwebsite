b4cb55370da7dce0fa8ec17381c72361d011be85
KashihakumwaJ <kas16003@byui.edu>
2020-05-17 18:11:07 -0600

    added more comments

--- server.js
+++ server.js
@@ -12,21 +12,26 @@ app.use(express.static('public'));
 // Setup enviroment variables
 const port = process.env.PORT || 3000;
 
+// this is the home route handler
 app.get('/',(req, res) =>{
     res.render('home');
 });
 
+// this is the about route handler
 app.get('/about',(req, res) =>{
     res.render('about');
 });
 
+// this is the resume route handler
 app.get('/resume',(req, res) =>{
     res.render('resume');
 });
 
+// this is the contact route handler
 app.get('/contact',(req, res) =>{
     res.render('contact');
 });
+
 // this is the portfolio route handler
 app.get('/portfolio',(req, res) =>{
     res.render('portfolio');
